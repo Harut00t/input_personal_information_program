8ff61179368cf0f326350546fcb00133d3257ffc
Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-10-21 13:45:30 +0800

    I have added the loop command for the name and age, the imported string is used to check if the name is only ascii letters and if the age is only digits. I also made the inputted age to be an integer for conditions.

--- input_personal_information_display_oldest_program.py
+++ input_personal_information_display_oldest_program.py
@@ -1,15 +1,53 @@
 #import string for name rules
+import string
+
+#character list for rules, variables
+letter = string.ascii_letters
+number = string.digits
+
+#print(letter)
 
 #initial variable for the program
+old_and_young = {}
 
 #loop for user inputs and to break 'yes' and 'no' reentry
+while True:
+    #2nd loop for another input the answers are 'yes' and 'no'
+    while True:
+        try:
+            #?loop for rules of names
+            while True:
+                name = input("Please input a name: ")
 
-#2nd loop for another input the answers are 'yes' and 'no'
+                if any(char in letter for char in name):
+                    break
+                else:
+                    #error message
+                    print("Only letters are allowed.")
+                    continue
+            
+            #?loop for rules of age
+            while True:
+                age = input("Please input the age: ")
+                
+                if any(char in number for char in age):
+                    num_age = int(age)
+                    if num_age < 130 and num_age >= 0:
+                            break
+                    else:
+                        print("Ages between 0 to 130 only.")
+                        continue
+                else:
+                     print("Only numbers are allowed.")
+                     continue
+                
+        except:
+            print("There's something wrong...")
 
-#?loop for rules of names
-    #error messages
-#?loop for rules of age
-    #error messages
+
+    
+
+    
 
 #an array of the name and age
             
