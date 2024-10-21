2e124fd2b914a3597abea634c5c0d100ba10ceae
Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-10-21 11:33:41 +0800

    This is the pseudocode for the initial, however this is incomplete and unfinished.

---
+++ input_personal_information_display_oldest_program.py
@@ -0,0 +1,26 @@
+#import string for name rules
+
+#initial variable for the program
+
+#loop for user inputs and to break 'yes' and 'no' reentry
+
+#2nd loop for another input the answers are 'yes' and 'no'
+
+#?loop for rules of names
+    #error messages
+#?loop for rules of age
+    #error messages
+
+#an array of the name and age
+            
+#print the name and age of oldest
+
+#new entries
+
+#stopping the loop
+
+
+
+
+
+
