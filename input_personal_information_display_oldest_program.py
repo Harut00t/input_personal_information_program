5fd7fd69672f65d74e0a6f8283a1c837f8fa30cb
Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-10-24 10:11:18 +0800

    I have removed the list and i used the dict.update for new dictionary, I also used for entry to iterate the value of the dictionary and used the max for the highest number. The variable oldest_person is the place holder and if the name has the highest number of age, then it will print the oldest and the age.

--- input_personal_information_display_oldest_program.py
+++ input_personal_information_display_oldest_program.py
@@ -3,12 +3,8 @@ import string
 
 #character list for rules, variables
 letter = string.ascii_letters
-number = string.digits
 
 #print(letter) #to check if it is working
-
-#create a list to handle all
-dicts = []
 
 #initial variable for the program
 name_and_age = {}
@@ -23,7 +19,7 @@ while True:
             while True:
                 name = input("Please input a name: ")
 
-                if any(char in letter for char in name):
+                if all(char in letter for char in name):
                     break
                 else:
                     #error message
@@ -35,18 +31,18 @@ while True:
                 age = input("Please input the age: ")
                 num_age = int(age)
                 
-                if any(char in number for char in age):
-                    if num_age < 130 and age != letter and num_age >= 0:
+                if age.isdigit():
+                    if 0 <= num_age < 130:
                         break #stop the loop
 
                     #error message
                     else:
                         print("Ages between 0 to 130 only.")
                         continue
-                #error message
-                else:
-                    print("Only numbers are allowed.")
-                    continue
+                # #error message
+                # else:
+                #     print("Only numbers are allowed.")
+                #     continue
                 
             #an array of the name and age
             name_and_age[name] = {
@@ -56,7 +52,7 @@ while True:
             
             print(name_and_age[name])
     
-            dicts.append(name_and_age)
+            name_and_age.update(name_and_age)
 
             #new entries
             another_entry = input("Another entry?: ")
@@ -65,8 +61,19 @@ while True:
             print("There's something wrong...")
 
     if another_entry == "n":
-        print(dicts)
-        print("complete!") #test
+        for entry in name_and_age.values():
+            # print(f"Name: {entry['name']}, Age: {entry['age']}") #a piece of code that prints the name and age
+            age_compare = [entry['age'] for entry in name_and_age.values()]
+            highest_age = max(age_compare)
+
+            oldest_person = None #To get the name of the highest age
+
+            for entry in name_and_age.values():
+                if entry['age'] == highest_age:
+                    oldest_person = entry['name']
+                    break
+
+        print(f"the oldest person is {oldest_person}, {highest_age}") #oldest person
         break #stopping the loop
 
     elif another_entry != "y":
